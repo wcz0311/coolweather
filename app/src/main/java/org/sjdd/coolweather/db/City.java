6845fdf9ad43a024e712036ef2c3091b89f6f227
wcz <wcz_nudt@163.com>
2017-05-22 18:16:45 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/org/sjdd/coolweather/db/City.java
@@ -0,0 +1,46 @@
+package org.sjdd.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by WCZ on 2017/5/22.
+ */
+
+public class City extends DataSupport {
+    private int id;
+    private String name;
+    private int code;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
